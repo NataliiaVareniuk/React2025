7f32342b50e6031488af58a2fbf3da2023c2ce3d
Nataliia Vareniuk <vnata3@gmail.com>
2026-02-19 11:34:15 +0100

    Change API base URL to production endpoint

--- src/shared/assets/api/api.js
+++ src/shared/assets/api/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = 'https://react-todo-api-vn-demo.onrender.com';
 import axios from 'axios';
 
 export const getTodoList = async () => {
